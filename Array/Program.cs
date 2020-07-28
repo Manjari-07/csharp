1a8468b3f7c98bfe0d321e5394fdfafbf40dabd6
manjari <manjari>
2020-07-28 12:51:17 +0530

    commited

---
+++ Array/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+
+        int[] numbers;
+        Program()
+        {
+            int size = 0;
+            Console.WriteLine("plz enter size");
+            size = Convert.ToInt32(Console.ReadLine());
+            numbers = new int[size];
+
+        }
+
+        void TakeNumber()
+        {
+            for (int i = 0; i <= numbers.Length; i++)
+            {
+                Console.WriteLine("plz enter the number{0}", (i + 1));
+
+                while (!Int32.TryParse(Console.ReadLine(), out numbers[i])) 
+                {
+                    Console.WriteLine("invalid num");
+                }
+
+            }
+        }
+        void print()
+        {
+            // for (int i = 0; i < size; i++)
+            //{
+            //  console.writeline(numbers[i]);
+            //}
+            Console.WriteLine("the no are");
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        void SortArray()
+        { int temp = 0;
+            for (int i=0;i<numbers.Length;i++)
+            {
+                for(int j=0;j<numbers.Length-1;j++)
+                {
+                    if(numbers[j]>numbers[j+1])
+                    {
+                        temp = numbers[j + 1];
+                        numbers[j + 1] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.SortArray();
+            p.TakeNumber();
+            p.print();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Array/student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class student
+    {
+        string[] skills;
+        string name, phone;
+        int id;
+        //indexer
+        public 
+        void someMethod(int id)
+        {
+            if (this.id==id)
+            {
+                Console.WriteLine("Same person");
+            }
+        }
+        public string this [int index]
+        {
+            get { return skills[index]; }
+            set { skills[index] = value; }
+        }
+        public string Name { get => name; set => name = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public int Id { get => id; set => id = value; }
+    }
+}
